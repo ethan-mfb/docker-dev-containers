c167006e5f76df571f71ebac1038ab86ab603626
Ethan Lemke <ethan@mfbtech.com>
2025-04-07 05:23:23 -0500

    fix the docker run command so it keeps the container alive

--- createAndRunRustDevContainer.sh
+++ createAndRunRustDevContainer.sh
@@ -3,4 +3,4 @@ if [ $? -ne 0 ]; then
   echo "Failed to build the image."
   exit 1
 fi
-docker run -d --name rust-dev-env -v $(pwd):/home/devuser -w /home/devuser rust-dev-env tail -f /dev/null
+docker run -d --name rust-dev-env -w /home/devuser -it rust-dev-env
